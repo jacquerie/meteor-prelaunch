1a066756f77870750bba227b1ea36a26f376976c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2013-08-03 13:32:35 +0200

    Update admin users to include just myself.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Emails = new Meteor.Collection("emails")
 EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // Github account usernames of admin users
-var ADMIN_USERS = ['niallo', 'peterbraden', 'willshulman'];
+var ADMIN_USERS = ['jacquerie'];
 function isAdmin(userId) {
   var user = Meteor.users.findOne({_id: userId});
   try {
